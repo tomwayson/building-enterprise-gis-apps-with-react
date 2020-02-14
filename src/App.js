8f0d0b0ac4cbe1fb94e232de78909b00daa746b4
Tom Wayson <tom@tomwayson.com>
2020-02-14 07:40:15 -0500

    get title from config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import { title } from './config/app';
 
 function App() {
   return (
     <>
       <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a className="navbar-brand" href="#">Enterprise ArcGIS App</a>
+        <a className="navbar-brand" href="#">{ title }</a>
       </nav>
-      <div className="container mt-5">
-      </div>
     </>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Enterprise ArcGIS App/i);
+test('renders the app title', () => {
+  const { getByText } = render(<App title="Title" />);
+  const linkElement = getByText(/Title/i);
   expect(linkElement).toBeInTheDocument();
 });
